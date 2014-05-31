d487ba5e5cf623523847a062675408278332cfeb
Ryan Smith <r@32k.io>
2014-05-31 13:15:55 -0700

    document public methods

--- lib/chain.rb
+++ lib/chain.rb
@@ -3,25 +3,42 @@ require 'net/https'
 require 'json'
 require 'thread'
 
+# A module that wraps the Chain HTTP API.
 module Chain
   @conn_mutex = Mutex.new
 
+  # Read's the URL from the environment.
+  # Include API token in user portion of UIR.
+  # E.g. https://API-TOKEN@api.chain.com
   API_URL = URI.parse(ENV['CHAIN_URL'] || raise("Must set CHAIN_URL."))
+  # A collection of root certificates used by api.chain.com
   CHAIN_PEM = File.expand_path('../chain.pem', __FILE__)
+  # Prefixed in the path of HTTP requests.
   API_VERSION = 'v1'
 
+  # Raised when an unexpected error occurs in either
+  # the HTTP request or the parsing of the response body.
   ChainError = Class.new(StandardError)
 
+  # Provide a bitcoin address.
+  # Returns a hash.
+  # Response details defined here: https://chain.com/#docs-address
   def self.get_address(addr)
     get("/#{API_VERSION}/bitcoin/addresses/#{addr}")
   end
 
+  # Provide a bitcion address.
+  # Returns an array of hashes.
+  # Response details defined here: https://chain.com/#docs-unspents
   def self.get_unspent_outputs(addr)
     get("/#{API_VERSION}/bitcoin/addresses/#{addr}/unspents")
   end
 
+  # Provide a hex encoded, signed transaction.
+  # Returns a string representing the newly created transaction hash.
   def self.send_transactions(hex)
-    put("/#{API_VERSION}/bitcoin/transactions", encode_body!({hex: hex}))
+    r = put("/#{API_VERSION}/bitcoin/transactions", encode_body!({hex: hex}))
+    r["transaction_hash"]
   end
 
   private
